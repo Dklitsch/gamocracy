481cb437ce310d2d8da98fbda4eb0d97ab49e263
DK <dklitsch@qualtrax.com>
2020-07-17 01:41:50 -0700

    Attribute validation for special characters on password.

--- Models/RegisterInputModel.cs
+++ Models/RegisterInputModel.cs
@@ -11,6 +11,7 @@ namespace gamocracy.Models
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [RegularExpression(@"^.*(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? ""]).*$", ErrorMessage="The {0} requires at least one lower case letter, one upper case letter, a number and a symbol.")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
