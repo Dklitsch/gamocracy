7e34683890a2a222f51503c01c1819be312d3ba6
DK <dklitsch@qualtrax.com>
2020-07-20 22:30:34 -0700

    Fix formatting, remove comment.

--- Startup.cs
+++ Startup.cs
@@ -61,19 +61,17 @@ namespace gamocracy
                 };
             });
 
-            services.AddCors(options =>
-        {
-            options.AddPolicy(name: CorsPolicy,
-                              builder =>
-                              {
-                                  builder.WithOrigins("http://localhost:3000")
-                                    .AllowAnyHeader()
-                                    .AllowAnyMethod();
-                              });
-        });
+            services.AddCors(options => 
+            {
+                options.AddPolicy(name: CorsPolicy, builder =>
+                    {
+                        builder.WithOrigins("http://localhost:3000")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
