641d14298f4a9d01d3f39a003a110573d7370f44
DK <dklitsch@qualtrax.com>
2020-07-21 22:30:05 -0700

    Add title to story

--- Models/Story.cs
+++ Models/Story.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
@@ -6,6 +7,8 @@ namespace gamocracy.Models
     public class Story
     {
         public int StoryId { get; set; }
+        [Required]
+        public string Title { get; set; }
         public string Summary { get; set; }
         public int StartingSceneId { get; set; }
         public Scene StartingScene { get; set; }
